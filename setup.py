64b66bf94279614f45b4b14be4ca75c21e6fff7b
Mukhtar ul Islam <mukhtar.ulislam@kitaab.biz>
2025-04-14 11:57:53 +0530

    patch:update the directory structure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='google-maps-scraper',
+    name='google-map-scraper',
     version='0.1.0',
     author='Mukhtar ul Islam',
     author_email='Mukhtarulislam88@hotmail.com',
